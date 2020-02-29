8c034988e9e9836be69b228d60fd6a7ad730afaf
Dan Pickett <dan.pickett@launchacademy.com>
2020-02-29 16:20:17 -0500

    support jest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,6 +24,19 @@ describe("generator-engage:app", () => {
           assert.noFile("app.js");
         });
     });
+
+    it("errors if I specify an invalid test-framework", () => {
+      return helpers
+        .run(path.join(__dirname, "../generators/app"))
+        .withOptions({ skipInstall: true, "test-framework": "badInput" })
+        .inTmpDir(dir => {
+          destinationRoot = dir;
+        })
+        .catch(err => {
+          expect(err.message).toMatch("Invalid view engine");
+          assert.noFile("app.js");
+        });
+    });
   });
 
   describe("happy path", () => {
@@ -121,6 +134,21 @@ describe("generator-engage:app", () => {
 
     it("adds a main.css", () => {
       assert.file("public/css/main.css");
+    });
+  });
+
+  describe("jest", () => {
+    it("adds jest as a devDependency", () => {
+      expect(json.devDependencies.jest).toBeDefined();
+    });
+
+    it("adds a jest.config.js", () => {
+      assert.file("jest.config.js");
+    });
+
+    it("adds scripts for testing and CI", () => {
+      expect(json.scripts.test).toEqual("jest");
+      expect(json.scripts.ci).toEqual("jest --coverage");
     });
   });
 
