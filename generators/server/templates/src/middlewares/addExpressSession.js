f576d98c5d8b38396db288d1a95489770e8f3c53
Dan Pickett <dpickett@users.noreply.github.com>
2020-06-21 15:17:31 -0400

    remove debugger
    fixes #42 
    
    what a n00b

--- generators/server/templates/src/middlewares/addExpressSession.js
+++ generators/server/templates/src/middlewares/addExpressSession.js
@@ -2,7 +2,6 @@ import session from "express-session";
 import configuration from "../config.js";
 
 const addExpressSession = app => {
-  debugger;
   app.use(
     session({
       secret: configuration.session.secret,
