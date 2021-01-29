9090e4bd865ee7803595845bc67ee6e556b64649
Brianna Kincart <briannakincart@gmail.com>
2021-01-28 16:06:31 -0800

    add Email format to User email

--- generators/server/templates/src/models/User.js
+++ generators/server/templates/src/models/User.js
@@ -29,7 +29,7 @@ class User extends uniqueFunc(Model) {
       required: ["email"],
 
       properties: {
-        email: { type: "string" },
+        email: { type: "string", format: "email" },
         cryptedPassword: { type: "string" },
       },
     };
