64ab7a44ccbd3a2e9e72fd3ba9845392d63d97ad
Nick Alberts <nwalberts5@gmail.com>
2023-04-18 17:10:21 -0400

    update auth handling to avoid blank pages

--- generators/client/templates/src/components/authentication/AuthenticatedRoute.js
+++ generators/client/templates/src/components/authentication/AuthenticatedRoute.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Redirect, Route } from "react-router";
 
 const AuthenticationCheck = ({ component: Component, user }) => {
+  if (user === undefined) {
+    return <div>Loading...</div>
+  }
   if (user !== null) {
-    return <Component />;
+    return <Component user={user} />;
   }
   return <Redirect to="/user-sessions/new" />;
 };
--- generators/server/templates/src/routes/clientRouter.js
+++ generators/server/templates/src/routes/clientRouter.js
@@ -5,6 +5,16 @@ const router = new express.Router();
 
 <% if(options["authentication"] === "passport") { -%>
 const clientRoutes = ["/", "/user-sessions/new", "/users/new"];
+const authedClientRoutes = ["/profile"];
+
+router.get(authedClientRoutes, (req, res) => {
+  if (req.user) {
+    res.sendFile(getClientIndexPath());
+  } else {
+    res.redirect("/user-sessions/new")
+  }
+});
+
 <% } else { -%>
 const clientRoutes = ["/client", "/name"];
 <% } -%>
